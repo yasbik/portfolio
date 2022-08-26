d91cad939e4b4890e95494460b5e2e1466a4367a
Yasin <yasin00sabik@gmail.com>
2022-08-26 15:48:41 -0400

    Project structure created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import Home from "./components/Home";
+import SocialLinks from "./components/SocialLinks";
 import NavBar from "./components/NavBar";
+import About from "./components/About";
+import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Contact from './components/Contact';
+
 
 function App() {
   return (
       <div>
         <NavBar />
+        <Home />
+        <About />
+        <Portfolio />
+        <Experience />
+        <Contact />
+        <SocialLinks />
       </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const About = () => {
+  return (
+  <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
+    <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+      <div className='pb-8'>
+        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
+      </div>
+
+      <p className='text-xl mt-20'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis consectetur dignissimos ipsam minus animi aliquam. Architecto ea ex, reprehenderit qui deleniti vitae enim error fugit quibusdam quae, eos, iure dicta.
+      </p>
+
+      <br />
+
+      <p className='text-xl'>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ad quasi sunt officia itaque blanditiis quos placeat tempore doloremque nulla, facere repellat praesentium, delectus aut molestias quae eaque suscipit minima!
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Pariatur, libero doloribus cupiditate reprehenderit voluptas harum, a cumque architecto assumenda quis laborum. Ipsam modi tempore dolore maxime quis inventore et voluptatum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis saepe maiores at labore debitis odit. Nihil, incidunt. Aliquid dolorem deleniti eius fugiat impedit culpa rerum nam, voluptatum iusto. Cum, ipsa.
+      </p>
+    </div>
+  </div>
+  );
+  
+};
+
+export default About;
