7af7501ffc94cbf619e6524ee7136a3c28e8c719
Abu Yasin Sabik <yasin00sabik@gmail.com>
2022-09-17 02:41:52 -0400

    Updated navbar background

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -64,7 +64,7 @@ const NavBar = () => {
 
       {/* Smooth scroll function */}
       {nav && (
-        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-zinc-800 text-gray-500">
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-slate-800 text-gray-500">
           {links.map(({id, link}) => (
             <li 
               key={id} 
